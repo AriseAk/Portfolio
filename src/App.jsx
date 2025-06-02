4660a8c541c6c1e51603fd6cd5e8b8d03bff9250
Akshay <ariseak13@gmail.com>
2025-06-02 18:57:07 +0530

    initial set up done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from './components/Navbar'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="text-center mt-10 text-xl font-bold text-blue-500">
+        Hello, I’m a Full Stack Developer!
+      </div>
+    </>
+  );
+}
+
+
+export default App;
